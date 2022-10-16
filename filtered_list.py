2dfd44ea6f8c43bfd99738ed02a36ce5fcd00169
katachi <mforemniak@gmail.com>
2022-10-16 22:33:18 +0200

    Added struct printer in tree

--- filtered_list.py
+++ filtered_list.py
@@ -1,5 +1,38 @@
 import tkinter as tk
+from tkinter import ttk
 import re
+
+class STR_B:
+    def __init__(self):
+        self.vals = {
+                    'a': 0,
+                    'b': 0,
+                    'c': 'Ala'
+                }
+
+
+class STR_A:
+    def __init__(self):
+        self.vals = {
+                    'a': STR_B(),
+                    'b': '',
+                    'c': [1,2,3,4]
+                }
+
+
+class TreeInserter:
+    def __init__(self, tree):
+        self.tree = tree
+
+    def insert_in_tree(self, val, name, parent=''):
+        if hasattr(val, "vals"):
+            p = self.tree.insert(parent, tk.END, text=name)
+            for node in val.vals:
+                self.insert_in_tree(val.vals[node], node, p)
+        else:
+            p = self.tree.insert(parent, tk.END, text=name, values=val)
+        self.tree.set(p, 0, value=14)
+
 
 class Field:
     def __init__(self, id, name):
@@ -30,8 +63,25 @@ class App(tk.Tk):
         self.lbl_selection = tk.Label(textvariable=self.selected_val)
         self.lbl_selection.pack()
 
+        self.tre = ttk.Treeview(columns=("field", "value"), show="tree")
+        self.tre.column('#0', width=120, stretch=0)
+        self.tre.column('#1', width=120, stretch=0)
+        TreeInserter(self.tre).insert_in_tree(STR_A(), 'STR_A')
+        self.tre.bind('<<TreeviewSelect>>', self.on_tre_selection_change)
+        self.tre.pack()
+
+        self.selected_node = tk.StringVar()
+        self.tre_selection = tk.Label(textvariable=self.selected_node)
+        self.tre_selection.pack()
+
     def on_lbx_selection_change(self, event):
         self.selected_val.set(''.join([self.lbx.get(i) for i in self.lbx.curselection()]))
+
+    def on_tre_selection_change(self, event):
+        node_id = self.tre.selection()[0]
+        node = self.tre.item(node_id)
+        p_node_txt = self.tre.item(self.tre.parent(node_id))['text']
+        self.selected_node.set(p_node_txt)
     
     def handle_change(self, *args):
         self.var.set(self.get_filtered_values(self.sv.get()))
